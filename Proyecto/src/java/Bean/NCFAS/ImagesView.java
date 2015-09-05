c34bb2c4702b3282550a65a8369dee1ccceb5cee
marceloestebanv <marcelo.verdugo.reyes@hotmail.com>
2015-09-05 13:02:18 -0300

    Agregado slider al inicio del CATA y al Inicio del NCFAS

--- Proyecto/src/java/Bean/NCFAS/ImagesView.java
+++ Proyecto/src/java/Bean/NCFAS/ImagesView.java
@@ -1,28 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    
 package Bean.NCFAS;
+
+
+ 
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-/**
- *
- * @author Marcelo Verdugo
- */
-
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+ 
 @ManagedBean
 public class ImagesView {
-    
-private List<String> images;
+     
+    private List<String> images;
      
     @PostConstruct
     public void init() {
+        
+        //obtener el real path del directorio
+     //    ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+ //String realPath=(String) servletContext.getRealPath("/")+"resources\\imagen\\";
         images = new ArrayList<String>();
-        for (int i = 1; i <= 4; i++) {
-            images.add("nature" + i + ".png");
+        for (int i = 1; i <= 10; i++) {
+            images.add(i +".jpg");
         }
     }
  
